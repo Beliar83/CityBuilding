84f6ecbb148c4e4e41a960e42a2a27ae7d3b706e
Karsten Bock <KarstenBock@gmx.net>
2018-11-26 07:00:51 +0100

    - Adjusted Test.

--- MessagingSystem.Akka.Test/EntityActorTest.cs
+++ MessagingSystem.Akka.Test/EntityActorTest.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using Akka.Actor;
 using Akka.TestKit.Xunit2;
@@ -15,29 +14,29 @@ namespace MessagingSystem.Akka.Test
     public class TestEntityActor : TestKit
     {
         [NotNull] private IActorRef entityActor;
-        [NotNull] private Dictionary<string, List<ReceiveDefinition>> receiveDefinitions;
+
+        [NotNull] private readonly Dictionary<string, List<ReceiveDefinition>> receiveDefinitions =
+            new Dictionary<string, List<ReceiveDefinition>>();
+
         [NotNull] private Entity entity;
 
         [Fact]
-        public void TestChangeState()
+        public void EntityActorChangesState()
         {
             GivenDummyEntityExists();
-            receiveDefinitions = new Dictionary<string, List<ReceiveDefinition>>
-            {
-                ["test"] = new List<ReceiveDefinition>
-                {
-                    new ReceiveDefinition(
-                        typeof(string),
-                        message => { TestActor.Tell("Successful", ActorRefs.NoSender); },
-                        message => true
-                    )
-                }
-            };
             GivenEntityActorExists();
+            receiveDefinitions["test"] = new List<ReceiveDefinition>
+            {
+                new ReceiveDefinition(
+                    typeof(string),
+                    message => { TestActor.Tell("Successful", ActorRefs.NoSender); },
+                    message => true
+                )
+            };
             entityActor.Tell(new ChangeState("test"));
             ExpectNoMsg(1000);
             entityActor.Tell("Test");
-            ExpectMsg<string>();
+            ExpectMsg<string>(s => s == "Successful");
         }
 
         private void GivenDummyEntityExists()
@@ -49,22 +48,6 @@ namespace MessagingSystem.Akka.Test
         private void GivenEntityActorExists()
         {
             entityActor = Sys.ActorOf(EntityActor.GetProps(entity, receiveDefinitions));
-        }
-
-
-        [ItemNotNull]
-        [NotNull]
-        private static List<Entity> CreateEntityList(int number)
-        {
-            var entities = new List<Entity>();
-            for (var i = 0; i < number; i++)
-            {
-                var entityMock = new Mock<Entity>();
-                entityMock.Setup(m => m.Id).Returns(Guid.NewGuid());
-                entities.Add(entityMock.Object);
-            }
-
-            return entities;
         }
     }
 }
